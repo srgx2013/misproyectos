7b337edfbbd58bb57b0b9fab55aad861e3014c9d
srgx2013 <srgx2012@gmail.com>
2024-09-19 16:37:18 -0600

    nueva nota

--- Angular/01-typescrip-intro/src/main.ts
+++ Angular/01-typescrip-intro/src/main.ts
@@ -1,7 +1,10 @@
+
+
+
 import './style.css'
 // import './topics/1-basic-types.ts';
-import './topics/03-funcions.ts'
-
+// import './topics/03-funcions.ts'
+import './topics/05-basic-destructuring'
 
 
 
