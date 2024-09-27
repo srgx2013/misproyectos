85d6ddb119a3c36d7f6aa9c1108e9cba577c8c26
srgx2013 <srgx2012@gmail.com>
2024-09-26 23:06:13 -0600

    nuevos componentes

---
+++ Angular/02-bases/src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+  public name: string = "iron man";
+  public age: number = 45;
+
+}
